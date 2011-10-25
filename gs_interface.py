492471f4357d5bf8929af333894936045e0f8353
Patrick W <d3non@ipad.(none)>
2011-10-25 18:11:40 +0200

    abstracted QueueThread

--- gs_interface.py
+++ gs_interface.py
@@ -1,12 +1,10 @@
 #this is an interface to submit flags to the gameserver
-import threading
+import lib.QueueThread as QueueThread
 import socket
-import Queue
 
-class GS_Interface(threading.Thread):
-	QueueMaxLen = 0
-
-	def __init__(self, srv_addr, srv_port, timeout = None, queue = None):
+class GS_Interface(QueueThread):
+	def __init__(self, srv_addr, srv_port, timeout = None, maxsize = 0):
+		QueueThread.__init__(maxsize)
 		self.__srv_addr = srv_addr
 		self.__srv_port = srv_port
 		if(timeout == None):
@@ -14,13 +12,6 @@ class GS_Interface(threading.Thread):
 		else
 			self.__timeout = timeout
 		self.__sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		if(queue == None):
-			self.__queue = Queue.Queue(GS_Interface.QueueMaxLen)
-		else:
-			self.__queue = queue
-
-	def _getQueue(self)
-		return self.__queue
 
 	def _open(self):
 		self.__sock.connect((self.__srv_addr,self.__srv_port))
@@ -29,21 +20,21 @@ class GS_Interface(threading.Thread):
 	def _submit(self,flag):
 		try
 			self.__sock.send(flag)
-		except socket.timeout, msg:
-			self.open()
+		except Exception:
+			self._open()
 			#print "connection timed out; reconnected and retransmitting"
-			self.__sock.send(flag)
-			continue
+			try
+				self.__sock.send(flag)
+			except Exception, e:
+				self.__sock.close()
+				raise e
 		return self.__sock.recv(1024)
 
 	def _close(self):
 		#here can come some proper connection closing if needed
 		self.__sock.close()
 
-	def put(self, item, block=True, timeout=None):
-		self.__queue.put(item,block,timeout)
-
-	def run(self)
+	def _process(self)
 		while True:
 			f = self.__queue.get()
 			f.setReturn(self._submit(f.getFlag()))
